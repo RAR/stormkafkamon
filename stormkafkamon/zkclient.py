5c9ee337d1671cff2faa7a34244e45cc1fcd4056
Andrew Rankin <andrew.rankin@appia.com>
2014-10-17 09:29:56 -0400

    update for newest kafka-python module and newer storm ZK setup.

--- stormkafkamon/zkclient.py
+++ stormkafkamon/zkclient.py
@@ -72,8 +72,8 @@ class ZkClient:
         try:
             for c in self.client.get_children(spout_root):
                 partitions = []
-                for p in self.client.get_children(self._zjoin([spout_root, c])):
-                    j = json.loads(self.client.get(self._zjoin([spout_root, c, p]))[0])
+                for p in self.client.get_children(spout_root):
+                    j = json.loads(self.client.get(self._zjoin([spout_root, c]))[0])
                     if j['topology']['name'] == topology:
                         partitions.append(j)
                 s.append(ZkKafkaSpout._make([c, partitions]))
